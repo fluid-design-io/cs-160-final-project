31586190777505e7aab78cb0a9b87471a8a37243
Jiuzhen Pan <2216991777@qq.com>
2022-08-04 12:38:45 -0700

    update layout

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -6,12 +6,13 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
-      <div className="text-3xl font-bold underline">Hello world!</div>
+    <div className="container mx-auto">
+      <div className="text-3xl font-bold underline mb-8">Hello world!</div>
       <h3 className="text-primary-default text-2xl font-logo">{name}</h3>
       <p>
         is a great place to start. Start with Ionic{" "}
         <a
+          className="text-tertiary-default mt-4"
           target="_blank"
           rel="noopener noreferrer"
           href="https://ionicframework.com/docs/components"
