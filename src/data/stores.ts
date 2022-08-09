0575b8976f778cefe39aa3d47580d4229c630f3f
Eunice <eunice.leee@gmail.com>
2022-08-09 00:58:41 -0700

    add some stores

--- src/data/stores.ts
+++ src/data/stores.ts
@@ -1,19 +1,87 @@
 export const stores = [
     {
         id: 1,
-        name: "Boba Store",
+        name: "Bon Bon Tea House",
         ratings: "4.9",
         description: "Boba, Tea, Desserts",
         filter: {
+            popular: true,
+            organic: true,
             biodegradable: false,
             compostable: false,
             recyclable: false,
-            popular: true,
             bringYourOwn: true,
-            organic: true,
         },
         images: ["https://www.theflouredcamera.com/wp-content/uploads/2021/06/01.jpg",
             "https://healthynibblesandbits.com/wp-content/uploads/2018/07/How-to-Make-Bubble-Tea-9.jpg",
             "https://ultimateomnoms.com/wp-content/uploads/2021/08/CFAA0712-3AB4-409B-B8BA-F652367681EE.jpg"]
+    },
+    {
+        id: 2,
+        name: "Chai Time",
+        ratings: "4.8",
+        description: "Tea, Desserts",
+        filter: {
+            popular: true,
+            organic: false,
+            biodegradable: true,
+            compostable: true,
+            recyclable: false,
+            bringYourOwn: true,
+        },
+        images: ["https://takestwoeggs.com/wp-content/uploads/2022/04/Jasmine-Milk-Tea-Takestwoeggs-Final-Photography-1.jpg",
+            "https://takestwoeggs.com/wp-content/uploads/2021/10/Thai-Iced-Tea-Takestwoeggs-FINAL-1.jpg",
+            "https://takestwoeggs.com/wp-content/uploads/2021/09/Matcha-Pineapple-Drink-Starbucks-inspired-Takestwoeggs-Final.jpg"]
+    },
+    {
+        id: 3,
+        name: "Boba Berkeley",
+        ratings: "4.6",
+        description: "Boba, Tea, Coffee",
+        filter: {
+            popular: false,
+            organic: false,
+            biodegradable: false,
+            compostable: true,
+            recyclable: false,
+            bringYourOwn: true,
+        },
+        images: ["https://takestwoeggs.com/wp-content/uploads/2022/01/How-to-Make-Vietnamese-Coffee-Ca%CC%80-Phe%CC%82-FINAL-2.jpg",
+            "https://takestwoeggs.com/wp-content/uploads/2022/05/Che-Thai-Takestwoeggs-Final-Photography-3.jpg",
+            "https://www.theflavorbender.com/wp-content/uploads/2020/06/Boba-Pearls-7340-1367x2048.jpg"]
+    },
+    {
+        id: 4,
+        name: "The Dessert Co.",
+        ratings: "4.8",
+        description: "Boba, Tea, Desserts",
+        filter: {
+            popular: true,
+            organic: false,
+            biodegradable: false,
+            compostable: false,
+            recyclable: true,
+            bringYourOwn: true,
+        },
+        images: ["https://lh3.googleusercontent.com/p/AF1QipMQA08SkiDOlWfWkSmwB7KN9K_p1SF8DY9BpsCn=s1280-p-no-v1",
+            "https://www.foxyfolksy.com/wp-content/uploads/2020/06/brown-sugar-milk-tea.jpg",
+            "https://lindseyeatsla.com/wp-content/uploads/2020/08/Lindseyeatsla_Matcha_Boba-7-1.jpg"]
+    },
+    {
+        id: 5,
+        name: "Infinitea",
+        ratings: "4.3",
+        description: "Boba, Tea",
+        filter: {
+            popular: false,
+            organic: false,
+            biodegradable: false,
+            compostable: false,
+            recyclable: true,
+            bringYourOwn: false,
+        },
+        images: ["https://takestwoeggs.com/wp-content/uploads/2021/06/Mangonada-Boba-Recipe-Chamoyada-takestwoeggs-Final.jpg",
+            "https://takestwoeggs.com/wp-content/uploads/2021/05/How-to-Make-homemade-boba-tapioca-pearls-takestwoeggs-final-3.jpg",
+            "https://realfood.tesco.com/media/images/RFO-1400x919-IcedCoffee-76221116-2565-4103-9899-89571028018e-0-1400x919.jpg"]
     },
 ]
