0a9393756399c966ca9c9ded7718f6cf8562611b
Jiuzhen Pan <2216991777@qq.com>
2022-08-09 20:06:53 -0700

    ✅ Done

--- src/components/RedeemAddPoints.tsx
+++ src/components/RedeemAddPoints.tsx
@@ -3,6 +3,8 @@ import {
   IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
   IonPage,
   IonTitle,
   IonToolbar,
@@ -26,11 +28,27 @@ function RedeemAddPoints({ onDissmiss }: { onDissmiss: () => void }) {
           </IonToolbar>
         </IonHeader>
         <div className="p-4">
-          {/* 
-            // TODO: Add form
-            // TODO: Add invite section
-        */}
-          <p>Add points section</p>
+          <IonItem>
+            <IonInput placeholder="Enter coupon code" />
+          </IonItem>
+          <IonButton expand="block" color="secondary">
+            Redeem
+          </IonButton>
+          <div className="text-xs text-medium-default text-center pt-8">----- OR -----</div>
+          <div className="w-3/5 max-w-xs p-4 mx-auto py-20">
+            <img
+              src="/assets/images/200-points.png"
+              className="w-full h-full"
+              alt=""
+            />
+          </div>
+          <IonButton expand="block" color="primary">
+            Invite
+          </IonButton>
+          <p className="text-xs text-medium-default pt-4">
+            For each new member joined and orered thier first drink, you and
+            your invited friend will both recieve 200 points. Max of 10 invites.
+          </p>
         </div>
       </IonContent>
     </IonPage>
